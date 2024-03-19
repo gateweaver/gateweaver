09925a315bd86e8221f7790e91dbd50a84588ded
Gladwin Dosunmu <gladwin.dos@hotmail.co.uk>
2024-03-19 21:23:12 +0000

    update logging in request

--- packages/server/src/middleware/http-logger.middleware.ts
+++ packages/server/src/middleware/http-logger.middleware.ts
@@ -2,12 +2,15 @@ import { Request, Response, NextFunction } from "express";
 import { logger } from "../utils";
 
 export const httpLogger = (req: Request, res: Response, next: NextFunction) => {
+  const query = Object.keys(req.query).length ? req.query : undefined;
+  const body = Object.keys(req.body).length ? req.body : undefined;
+
   logger.info({
     message: "Incoming request",
     method: req.method,
     path: req.path,
-    query: req.query,
-    body: req.body,
+    query,
+    body,
   });
   next();
 };

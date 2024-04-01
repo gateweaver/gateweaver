0720e6ae85ab76caf8ff14bcd2b21133c1e943a9
Gladwin Dosunmu <gladwin.dos@hotmail.co.uk>
2024-04-01 14:15:00 +0100

    update linting

--- packages/policies/src/rate-limit/key-generator.ts
+++ packages/policies/src/rate-limit/key-generator.ts
@@ -11,7 +11,7 @@ export class RateLimitUnauthorizedError extends Error {
 const decodeJwt = (token: string) => {
   try {
     return JSON.parse(atob(token.split(".")[1]));
-  } catch (e) {
+  } catch (_) {
     throw new RateLimitUnauthorizedError("Invalid Token");
   }
 };

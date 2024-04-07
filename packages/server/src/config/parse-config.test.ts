4816c3efece4123d4b038028c2d0aef4e79824fb
Gladwin <gladwin.dos@hotmail.co.uk>
2024-04-07 16:20:48 +0100

    update parse env and docker image (#56)

--- packages/server/src/config/parse-config.test.ts
+++ packages/server/src/config/parse-config.test.ts
@@ -1,16 +1,14 @@
 import fs from "fs";
 import { parseConfig } from "./parse-config";
-import { validateConfig } from "./validate-config";
+import { InvalidConfigError, validateConfig } from "./validate-config";
 
 jest.mock("fs");
 jest.mock("./validate-config");
 
 describe("parseConfig", () => {
-  const mockEnv = { TEST_VAR: "value" };
-
   beforeEach(() => {
     jest.resetAllMocks();
-    process.env = { ...mockEnv }; // Reset environment variables for each test
+    process.env.TEST_VAR = "test value";
   });
 
   it("should correctly identify and parse a file with .yaml extension", () => {
@@ -41,7 +39,7 @@ describe("parseConfig", () => {
   it("should replace environment variables in the file content before validating", () => {
     const configPath = "config";
     const fullPath = `${configPath}.yml`;
-    const mockConfig = { endpoints: "value" };
+    const mockConfig = { endpoints: "test value" };
 
     (fs.existsSync as jest.Mock).mockReturnValue(true);
     (fs.readFileSync as jest.Mock).mockReturnValue("endpoints: ${TEST_VAR}");
@@ -50,5 +48,14 @@ describe("parseConfig", () => {
 
     expect(fs.readFileSync).toHaveBeenCalledWith(fullPath, "utf8");
     expect(validateConfig).toHaveBeenCalledWith(mockConfig);
+  });
+
+  it("should throw an InvalidConfigError if an environment variable is not found", () => {
+    const configPath = "config";
+
+    (fs.existsSync as jest.Mock).mockReturnValue(true);
+    (fs.readFileSync as jest.Mock).mockReturnValue("endpoints: ${MISSING_VAR}");
+
+    expect(() => parseConfig(configPath)).toThrow(InvalidConfigError);
   });
 });

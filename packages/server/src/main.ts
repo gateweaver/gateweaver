0fabb807c21d729d0fe37d583cb5c7de2522807b
Gladwin Dosunmu <gladwin.dos@hotmail.co.uk>
2024-04-08 16:45:22 +0100

    update main

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,14 +1,11 @@
 import { config } from "dotenv";
 config({ path: ".env.gateweaver" });
 import { getConfigPath } from "./config/get-config-path";
-import { logger, startServer } from ".";
+import { startServer } from "./server";
+import { logger } from "./logger";
 
 try {
   const filePath = getConfigPath();
-
-  if (!filePath) {
-    throw new Error("Gateweaver config file not found.");
-  }
 
   startServer(filePath);
 } catch (error) {

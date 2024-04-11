ff7911f99bad4355281f3846e388349e6e8c2fd3
Gladwin <gladwin.dos@hotmail.co.uk>
2024-04-11 23:58:13 +0100

    Add setup tests (#61)
    * add setup-proxy tests
    
    * create setup policies tests

--- packages/server/src/router/setup/setup-proxy.ts
+++ packages/server/src/router/setup/setup-proxy.ts
@@ -9,6 +9,26 @@ import { PolicyOption } from "@gateweaver/policies";
 import { Endpoint } from "../../config/config.types";
 import { logger } from "../../logger";
 
+const RATE_LIMIT_HEADERS = [
+  "X-Ratelimit-Limit",
+  "X-Ratelimit-Remaining",
+  "X-Ratelimit-Reset",
+  "RateLimit-Policy",
+  "RateLimit-Limit",
+  "RateLimit-Remaining",
+  "RateLimit-Reset",
+  "RateLimit",
+];
+
+const CORS_HEADERS = [
+  "Access-Control-Allow-Origin",
+  "Access-Control-Allow-Credentials",
+  "Access-Control-Allow-Methods",
+  "Access-Control-Allow-Headers",
+  "Access-Control-Expose-Headers",
+  "Access-Control-Max-Age",
+];
+
 const deleteHeaders = (res: IncomingMessage, headers: string[]): void => {
   headers.forEach((header) => {
     delete res.headers[header];
@@ -16,59 +36,47 @@ const deleteHeaders = (res: IncomingMessage, headers: string[]): void => {
   });
 };
 
+export const buildTargetUrl = (endpoint: Endpoint) => {
+  let url = endpoint.target.url;
+
+  if (endpoint.request?.query) {
+    const query = new URLSearchParams(endpoint.request.query);
+    url += `?${query}`;
+  }
+
+  return url;
+};
+
+export const onProxyRes = (
+  proxyRes: IncomingMessage,
+  _req: Request,
+  _res: Response,
+  endpoint: Endpoint,
+) => {
+  proxyRes.headers = {
+    ...proxyRes.headers,
+    ...endpoint.response?.headers,
+  };
+
+  // Remove headers with empty values
+  Object.keys(proxyRes.headers).forEach((key) => {
+    if (!proxyRes.headers[key]) {
+      delete proxyRes.headers[key];
+    }
+  });
+
+  // Remove rate limit headers to avoid conflicts with rate limit middleware
+  if (endpoint.policies?.includes(PolicyOption.RateLimit)) {
+    deleteHeaders(proxyRes, RATE_LIMIT_HEADERS);
+  }
+
+  // Remove CORS headers to avoid conflicts with CORS middleware
+  if (endpoint.policies?.includes(PolicyOption.Cors)) {
+    deleteHeaders(proxyRes, CORS_HEADERS);
+  }
+};
+
 export const setupProxy = (router: Router, endpoint: Endpoint): void => {
-  const buildTargetUrl = () => {
-    let url = endpoint.target.url;
-
-    if (endpoint.request?.query) {
-      const query = new URLSearchParams(endpoint.request.query);
-      url += `?${query}`;
-    }
-
-    return url;
-  };
-
-  const onProxyRes = (
-    proxyRes: IncomingMessage,
-    _req: Request,
-    _res: Response,
-  ) => {
-    proxyRes.headers = {
-      ...proxyRes.headers,
-      ...endpoint.response?.headers,
-    };
-
-    Object.keys(proxyRes.headers).forEach((key) => {
-      if (!proxyRes.headers[key]) {
-        delete proxyRes.headers[key];
-      }
-    });
-
-    if (endpoint.policies?.includes(PolicyOption.RateLimit)) {
-      deleteHeaders(proxyRes, [
-        "X-Ratelimit-Limit",
-        "X-Ratelimit-Remaining",
-        "X-Ratelimit-Reset",
-        "RateLimit-Policy",
-        "RateLimit-Limit",
-        "RateLimit-Remaining",
-        "RateLimit-Reset",
-        "RateLimit",
-      ]);
-    }
-
-    if (endpoint.policies?.includes(PolicyOption.Cors)) {
-      deleteHeaders(proxyRes, [
-        "Access-Control-Allow-Origin",
-        "Access-Control-Allow-Credentials",
-        "Access-Control-Allow-Methods",
-        "Access-Control-Allow-Headers",
-        "Access-Control-Expose-Headers",
-        "Access-Control-Max-Age",
-      ]);
-    }
-  };
-
   const logProvider = () => {
     return {
       debug: logger.debug.bind(logger),
@@ -80,13 +88,14 @@ export const setupProxy = (router: Router, endpoint: Endpoint): void => {
   };
 
   const proxyOptions: Options = {
-    target: buildTargetUrl(),
+    target: buildTargetUrl(endpoint),
     changeOrigin: true,
     pathRewrite: { [`^${endpoint.path}`]: "" },
     headers: endpoint.request?.headers,
     xfwd: true,
     onProxyReq: fixRequestBody,
-    onProxyRes,
+    onProxyRes: (proxyRes, req, res) =>
+      onProxyRes(proxyRes, req, res, endpoint),
     logProvider,
     logLevel: "error",
   };

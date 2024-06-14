6e3cc87945a6262bed4f0876a79db0fcda27ba3a
Gladwin <gladwin.dos@hotmail.co.uk>
2024-06-14 11:17:49 +0100

    Add custom middleware (#69)
    * create dev folder
    
    * add custom middleware setup
    
    * update dev files
    
    * remove env var

--- packages/server/src/router/index.ts
+++ packages/server/src/router/index.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { Config } from "../config/config.types";
 import { setupEndpoints } from "./setup/setup-endpoints";
 
-export const createRouter = (config: Config): Router => {
+export const createRouter = async (config: Config): Promise<Router> => {
   const router = Router();
 
-  setupEndpoints(router, config);
+  await setupEndpoints(router, config);
 
   return router;
 };

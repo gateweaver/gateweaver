0b5c8f024b0ca693005e967067393e02b77d3a33
Gladwin <gladwin.dos@hotmail.co.uk>
2024-06-22 14:44:32 +0100

    Policy definitions optional (#73)
    * Allow policies to be used with defaults without being defined in config
    
    * update parseConfig tests
    
    * update policies logs
    
    * update parseConfig

--- packages/server/src/router/setup/global/policies.ts
+++ packages/server/src/router/setup/global/policies.ts
@@ -4,6 +4,7 @@ import {
   PolicyOption,
   policyMiddleware,
 } from "@gateweaver/policies";
+import { logger } from "../../../utils/logger";
 
 interface SetupGlobalPoliciesParams {
   router: Router;
@@ -21,18 +22,22 @@ export const setupGlobalPolicies = ({
 
     if (cors && policies.includes(PolicyOption.Cors)) {
       router.use(policyMiddleware.cors(cors));
+      logger.info(`Applied CORS policy to all endpoints`);
     }
 
     if (jwt && policies.includes(PolicyOption.Jwt)) {
       router.use(policyMiddleware.jwt(jwt));
+      logger.info(`Applied JWT policy to all endpoints`);
     }
 
     if (apiKey && policies.includes(PolicyOption.ApiKey)) {
       router.use(policyMiddleware.apiKey(apiKey));
+      logger.info(`Applied API key policy to all endpoints`);
     }
 
     if (rateLimit && policies.includes(PolicyOption.RateLimit)) {
       router.use(policyMiddleware.rateLimit(rateLimit));
+      logger.info(`Applied rate limit policy to all endpoints`);
     }
   }
 };

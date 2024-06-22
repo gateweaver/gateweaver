396137ad3865644b84f7df0fd374bb4ab9a65c63
Gladwin <gladwin.dos@hotmail.co.uk>
2024-06-22 16:20:08 +0100

    Update e2e tests (#74)
    * add customer handler e2e test
    
    * add e2e tests for custom middleware

--- packages/e2e/jest.config.ts
+++ packages/e2e/jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/jest.env.ts", "<rootDir>/jest.setup.ts"],
+  transform: {},
 };
 
 export default config;

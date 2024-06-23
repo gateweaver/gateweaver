5bed7c4794f47f9a5c2b621274b2ba12d97acc1a
Gladwin Dosunmu <gladwin.dos@hotmail.co.uk>
2024-06-23 14:28:58 +0100

    update logging

--- packages/server/src/router/setup/endpoints/policies.ts
+++ packages/server/src/router/setup/endpoints/policies.ts
@@ -25,22 +25,22 @@ export const setupEndpointPolicies = ({
 
     if (cors && policies.includes(PolicyOption.Cors)) {
       router.use(endpoint.path, policyMiddleware.cors(cors));
-      logger.info(`Applied CORS policy to ${endpoint.path}`);
+      logger.info(`Applied CORS policy to endpoint ${endpoint.path}`);
     }
 
     if (jwt && policies.includes(PolicyOption.Jwt)) {
       router.use(endpoint.path, policyMiddleware.jwt(jwt));
-      logger.info(`Applied JWT policy to ${endpoint.path}`);
+      logger.info(`Applied JWT policy to endpoint ${endpoint.path}`);
     }
 
     if (apiKey && policies.includes(PolicyOption.ApiKey)) {
       router.use(endpoint.path, policyMiddleware.apiKey(apiKey));
-      logger.info(`Applied API key policy to ${endpoint.path}`);
+      logger.info(`Applied API key policy to endpoint ${endpoint.path}`);
     }
 
     if (rateLimit && policies.includes(PolicyOption.RateLimit)) {
       router.use(endpoint.path, policyMiddleware.rateLimit(rateLimit));
-      logger.info(`Applied rate limit policy to ${endpoint.path}`);
+      logger.info(`Applied rate limit policy to endpoint ${endpoint.path}`);
     }
   }
 };
